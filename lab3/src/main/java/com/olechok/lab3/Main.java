6850ab8067e8901b9164b2ec7ffe30eff7915782
OlhaMish <olgami1402@gmail.com>
2024-11-04 22:08:41 +0200

    feat: add Main class with furniture array sorting and search functionality

---
+++ lab3/src/main/java/com/olechok/lab3/Main.java
@@ -0,0 +1,77 @@
+package com.olechok.lab3;
+
+//Створити клас із виконавчим методом, в якому створити масив з об’єктів
+//класу меблі, який складається як мінімум з 5-и полів.
+//Відсортувати масив, за одним з полів за зростанням,
+//а за іншим — за спаданням, використовуючи при цьому стандартні засоби сортування ().
+//Після цього знайти в масиві об’єкт, який ідентичний заданому.
+//Всі змінні повинні бути описані та значення їх задані у виконавчому методі.
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * The Main class creates an array of Furniture objects and demonstrates sorting and searching within the array.
+ * - Sorts the array of furniture items first by price (ascending) and then by material (descending).
+ * - Searches for a specific Furniture object in the sorted array.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        // Create an array of Furniture objects
+        Furniture[] furniture = new Furniture[5];
+        furniture[0] = new Furniture("Royal Table",
+                "Gold",
+                "Golden",
+                1000.90,
+                2);
+
+        furniture[1] = new Furniture("Crazy Sofa",
+                "Velvet",
+                "Purple",
+                290.50,
+                20);
+
+        furniture[2] = new Furniture("Cute Chair",
+                "Wood",
+                "Green",
+                50.00,
+                110);
+
+        furniture[3] = new Furniture("Majestic Cupboard",
+                "Wood",
+                "White",
+                210.90,
+                12);
+
+        furniture[4] = new Furniture("Cozy Bed",
+                "Wood",
+                "Blue",
+                290.50,
+                12);
+
+        // Sort the array by price in ascending order, then by material in descending order
+        Arrays.sort(furniture,
+                Comparator.comparing(Furniture::getPrice)
+                .thenComparing(Comparator.comparing(Furniture::getMaterial).reversed()));
+
+        // Display sorted furniture array
+        System.out.println("Sorted Furniture Array:");
+        for (Furniture oneFurniture : furniture) {
+            System.out.println(oneFurniture);
+        }
+
+        // Create a Furniture object to search for within the sorted array
+        Furniture furnitureToFind = new Furniture("Crazy Sofa",
+                "Velvet",
+                "Purple",
+                290.50,
+                20);
+
+        // Check if the specified Furniture object exists in the array
+        boolean found = Arrays.asList(furniture).contains(furnitureToFind);
+        System.out.println("\nIs target furniture found? " + found);
+
+    }
+
+}
