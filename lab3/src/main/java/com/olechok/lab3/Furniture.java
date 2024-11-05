06e6dc71b3b608fb6254cd34b6f5119a992e8e89
OlhaMish <olgami1402@gmail.com>
2024-11-05 20:51:24 +0200

    style: add more Javadoc comments for improved documentation for lab3

--- lab3/src/main/java/com/olechok/lab3/Furniture.java
+++ lab3/src/main/java/com/olechok/lab3/Furniture.java
@@ -1,34 +1,35 @@
 package com.olechok.lab3;
 
-
 import java.util.Objects;
 
 /**
  * The Furniture class represents a furniture item with several attributes:
- * - name: The name of the furniture item
- * - material: The material used to make the furniture item
- * - color: The color of the furniture item
- * - price: The price of the furniture item
- * - amount: The quantity of the furniture item in stock
+ * <ul>
+ *     <li><b>name</b>: The name of the furniture item</li>
+ *     <li><b>material</b>: The material used to make the furniture item</li>
+ *     <li><b>color</b>: The color of the furniture item</li>
+ *     <li><b>price</b>: The price of the furniture item</li>
+ *     <li><b>amount</b>: The quantity of the furniture item in stock</li>
+ * </ul>
  *
  * The class provides getters for each field, an equals method for comparison,
  * and a toString method for formatted output.
  */
 public class Furniture {
-    String name;
-    String material;
-    String color;
-    double price;
-    int amount;
+    private String name;
+    private String material;
+    private String color;
+    private double price;
+    private int amount;
 
     /**
      * Constructs a Furniture object with specified attributes.
      *
-     * @param name The name of the furniture item
-     * @param material The material of the furniture item
-     * @param color The color of the furniture item
-     * @param price The price of the furniture item
-     * @param amount The amount of the furniture item in stock
+     * @param name     The name of the furniture item.
+     * @param material The material of the furniture item.
+     * @param color    The color of the furniture item.
+     * @param price    The price of the furniture item.
+     * @param amount   The quantity of the furniture item in stock.
      */
     public Furniture(String name, String material, String color, double price, int amount) {
         this.name = name;
@@ -58,6 +59,12 @@ public class Furniture {
         return amount;
     }
 
+    /**
+     * Indicates whether some other object is "equal to" this furniture.
+     *
+     * @param o The reference object with which to compare.
+     * @return true if this furniture is the same as the obj argument; false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -70,20 +77,29 @@ public class Furniture {
                 material.equals(furniture.material);
     }
 
+    /**
+     * Returns a hash code value for the object, useful in hash tables.
+     *
+     * @return A hash code value for this furniture.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(name, material, color, price, amount);
     }
 
+    /**
+     * Returns a string representation of the furniture item.
+     *
+     * @return A string representation of the furniture, including its attributes.
+     */
     @Override
     public String toString() {
         return "Furniture{" +
                 "name='" + name + '\'' +
                 ", material='" + material + '\'' +
-                ", color=" + color +
+                ", color='" + color + '\'' +
                 ", price=" + price +
                 ", amount=" + amount +
                 '}';
     }
-
 }
